1993f01dbab0cde4854d65cbefb347bb3ea7ba5f
George Higbie <gehigbie@gmail.com>
2017-02-02 16:58:45 -0700

    added fragment transaction to toppings list

--- app/src/main/java/layout/ToppingsStart.java
+++ app/src/main/java/layout/ToppingsStart.java
@@ -2,9 +2,12 @@ package layout;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.geogehigbie.pizzabuilder.R;
 
@@ -15,13 +18,24 @@ public class ToppingsStart extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_toppings_start, container, false);
+
 
         addOnClickListeners();
+
+        return inflater.inflate(R.layout.fragment_toppings_start, container, false);
     }
 
 
     private void addOnClickListeners(){
+        Button done = (Button) getActivity().findViewById(R.id.toppingsDone);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.fragment_container, new ToppingsList());
+            }
+        });
 
     }
 
