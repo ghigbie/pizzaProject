7983bd77650d729e9cdbe72b35dd45b4bc0713cd
George Higbie <gehigbie@gmail.com>
2017-02-02 18:55:26 -0700

    added another custom list adapter

---
+++ app/src/main/java/com/geogehigbie/pizzabuilder/CustomListAdapterToppings.java
@@ -0,0 +1,12 @@
+package com.geogehigbie.pizzabuilder;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by georgehigbie on 2/2/17.
+ */
+
+public class CustomListAdapterToppings extends ArrayAdapter<String> {
+
+
+}
