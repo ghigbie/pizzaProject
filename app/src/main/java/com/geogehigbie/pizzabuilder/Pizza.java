d61aa44aae283b7a0a0ec4026ce82d2dcd36f015
George Higbie <gehigbie@gmail.com>
2017-02-06 16:10:26 -0700

    added pizza variables

--- app/src/main/java/com/geogehigbie/pizzabuilder/Pizza.java
+++ app/src/main/java/com/geogehigbie/pizzabuilder/Pizza.java
@@ -1,10 +1,16 @@
 package com.geogehigbie.pizzabuilder;
 
+import java.util.ArrayList;
+
 /**
  * Created by georgehigbie on 2/6/17.
  */
 
 public class Pizza {
+
+    String name;
+    ArrayList listOfToppings;
+
 
 
 }
