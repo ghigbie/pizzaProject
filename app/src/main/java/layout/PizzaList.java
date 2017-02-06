18f64741594cb14c6a4068ef9bc6c4b6952c7072
George Higbie <gehigbie@gmail.com>
2017-02-06 15:45:50 -0700

    changed pizza list adapter

--- app/src/main/java/layout/PizzaList.java
+++ app/src/main/java/layout/PizzaList.java
@@ -5,16 +5,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.geogehigbie.pizzabuilder.R;
 
+import java.util.ArrayList;
+
 public class PizzaList extends Fragment {
+
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_pizza_list, container, false);
+
+        ArrayList<String> pizzaArrayList = getArguments().getStringArrayList("pizzaArraysList");
+        ListView listView =
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pizza_list, container, false);
+        return view;
     }
 
 
