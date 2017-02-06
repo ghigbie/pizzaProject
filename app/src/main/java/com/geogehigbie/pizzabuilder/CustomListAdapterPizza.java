182b997e33ea57632966ad65ca7fc7d409fa14f9
George Higbie <gehigbie@gmail.com>
2017-02-06 15:33:25 -0700

    added methods to CustomListAdapterPizza

--- app/src/main/java/com/geogehigbie/pizzabuilder/CustomListAdapterPizza.java
+++ app/src/main/java/com/geogehigbie/pizzabuilder/CustomListAdapterPizza.java
@@ -1,7 +1,13 @@
 package com.geogehigbie.pizzabuilder;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,7 +22,33 @@ public class CustomListAdapterPizza extends ArrayAdapter<String> {
         super(context, 0, values);
     }
 
+    public View getView(int position, View convertView, ViewGroup parent){
+        String pizzaInList = getItem(position);
 
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.pizza_list_layout, parent, false);
+        }
+
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        String itemInList = getItem(position);
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.topping_list_layout, parent, false);
+        }
+
+        TextView textView = (TextView) convertView.findViewById(R.id.textViewCustomToppings);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageViewCustomToppings);
+
+        textView.setText(itemInList);
+        imageView.setImageResource(R.drawable.mushroom);
+
+        return convertView;
+    }
 
 }
 
