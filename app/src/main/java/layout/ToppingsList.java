ee8a223eb41673c7e59850a0f7eae87d742c4172
George Higbie <gehigbie@gmail.com>
2017-02-06 14:16:15 -0700

    made small changes

--- app/src/main/java/layout/ToppingsList.java
+++ app/src/main/java/layout/ToppingsList.java
@@ -21,6 +21,7 @@ public class ToppingsList extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
+
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_toppings_list, container, false);
 
