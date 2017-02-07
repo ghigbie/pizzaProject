d5bc25c12c7c7c1c564175e753fca381deff1b2d
George Higbie <gehigbie@gmail.com>
2017-02-06 20:52:54 -0700

    added button to layout and created onclick listeners

--- app/src/main/java/layout/PizzaSaveAndName.java
+++ app/src/main/java/layout/PizzaSaveAndName.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.geogehigbie.pizzabuilder.R;
 
@@ -21,6 +24,29 @@ public class PizzaSaveAndName extends Fragment {
         return view;
 
     }
+
+    public void setClickListener(){
+        Button button = (Button) view.findViewById(R.id.enter_pizza_name_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getPizzaName();
+            }
+        });
+    }
+
+    public void getPizzaName() {
+
+        EditText editText = (EditText) view.findViewById(R.id.edit_text_pizza_name);
+        if (editText != null) {
+            String pizzaName = editText.getText().toString();
+        } else {
+            Toast toast = Toast.makeText(getActivity().getApplicationContext(), "Please give this pizza a name.", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+
 
 
 }
