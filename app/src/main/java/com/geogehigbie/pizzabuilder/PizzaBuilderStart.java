503b788504b8ed9181b9ad0dc97b985716c912d6
George Higbie <gehigbie@gmail.com>
2017-02-06 17:03:55 -0700

    added code to hide an show buttons and to get text

--- app/src/main/java/com/geogehigbie/pizzabuilder/PizzaBuilderStart.java
+++ app/src/main/java/com/geogehigbie/pizzabuilder/PizzaBuilderStart.java
@@ -8,9 +8,13 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import layout.PizzaList;
 import layout.ToppingsStart;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 /**
  * Created by georgehigbie on 1/31/17.
@@ -38,16 +42,29 @@ public class PizzaBuilderStart extends AppCompatActivity {
 
         Button enterButton = (Button) findViewById(R.id.enter_start);
        // Button noButton = (Button) findViewById(R.id.no_start);
-        Button seeListPizzasButton = (Button) findViewById(R.id.seeListOfPizza);
+        final Button seeListPizzasButton = (Button) findViewById(R.id.seeListOfPizza);
 
-        enterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fragmentTransaction.add(R.id.fragment_container, new ToppingsStart());
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
-            }
-        });
+        EditText editText = (EditText) findViewById(R.id.edit_text_pizza_name);
+        if(editText != null) {
+            enterButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    EditText editText = (EditText) findViewById(R.id.edit_text_pizza_name);
+                    String name = editText.getText().toString();
+
+                    seeListPizzasButton.setVisibility(View.VISIBLE);
+
+
+                    Pizza pizzaNew = new Pizza(name, )
+
+                    fragmentTransaction.add(R.id.fragment_container, new ToppingsStart());
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                }
+            });
+        }else{
+            Toast toast = Toast.makeText(this, "Please give your pizza a name to begin", Toast.LENGTH_SHORT);
+        }
 
 //        noButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
