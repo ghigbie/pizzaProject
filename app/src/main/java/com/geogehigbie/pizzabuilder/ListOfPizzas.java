7fd3cc14ae6941d234ab8c2d67910f70a6d8a3ee
George Higbie <gehigbie@gmail.com>
2017-02-07 00:50:34 -0700

    created new onclick listener

--- app/src/main/java/com/geogehigbie/pizzabuilder/ListOfPizzas.java
+++ app/src/main/java/com/geogehigbie/pizzabuilder/ListOfPizzas.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -26,6 +28,16 @@ public class ListOfPizzas extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.pizzaListView);
         CustomListAdapterPizza listAdapterPizza = new CustomListAdapterPizza(this, pizzaNamesArrayList);
         listView.setAdapter(listAdapterPizza);
+    }
+
+    public void setOnClickListener(){
+        Button button = (Button) findViewById(R.id.createNewPizza1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 }
