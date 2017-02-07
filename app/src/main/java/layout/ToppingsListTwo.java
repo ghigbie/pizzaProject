9c0c82e370d6f253e36629bc8627f6f6aa867c69
George Higbie <gehigbie@gmail.com>
2017-02-07 01:22:23 -0700

    changed layout and changeed ToppingslistTwo class

--- app/src/main/java/layout/ToppingsListTwo.java
+++ app/src/main/java/layout/ToppingsListTwo.java
@@ -5,8 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
+import com.geogehigbie.pizzabuilder.CustomListAdapterToppings;
 import com.geogehigbie.pizzabuilder.R;
+
+import java.util.ArrayList;
 
 
 public class ToppingsListTwo extends Fragment {
@@ -17,6 +22,14 @@ public class ToppingsListTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_toppings_list_two, container, false);
+
+        ArrayList<String> toppingsArrayList = getArguments().getStringArrayList("toppingsArraysList");
+
+
+        ListView listView = (ListView) view.findViewById(R.id.toppingsListViewTwo);
+        ListAdapter listAdapter = new CustomListAdapterToppings(getActivity().getApplicationContext(), toppingsArrayList);
+
+        listView.setAdapter(listAdapter);
 
         return view;
     }
